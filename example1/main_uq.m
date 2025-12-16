677269675a0725f61cbae517bf92d7832a3e7632
Valentin Breaz <valentin.breaz@bristol.ac.uk>
2025-12-16 15:00:31 +0000

    surrogates for modal frequencies verus pitch angle

--- example1/main_uq.m
+++ example1/main_uq.m
@@ -150,3 +150,78 @@ for ii = 1:length(alp)
     end
 end    
 set(0, 'DefaultFigureVisible', 'on');
+
+%% 4. Surrogates for the modal frequencies (uncertain variable: pitch angle)
+% Description of the uncertain variables for UQLab
+InputOpts.Marginals(1).Type = 'Uniform';
+InputOpts.Marginals(1).Parameters = [0, pi/2]; % (pitch angle) lower and upper uncertainty bound
+myInput = uq_createInput(InputOpts); 
+alp = linspace(60*1e9,80*1e9,3);  %ranges of Young's modulus E
+
+set(0, 'DefaultFigureVisible', 'off');
+surrogates_modal_frequencies = cell(length(alp), 1);
+
+for ii = 1:length(alp)
+    % Description of the physical model for UQLab
+    ModelOpts.mFile = 'model_evals_windoff_uncertain_pitch';
+    ModelOpts.isVectorized = false;
+    ModelOpts.Parameters = [alp(ii)];
+    myModel_evals_windoff = uq_createModel(ModelOpts);
+
+    N_train = 5;                                            % initial training set size (the set will be updated until the surrogate validation error is low enough)
+    MetaOpts.Type = 'Metamodel';                            % 'metamodel': another word for 'surrogate'    
+    MetaOpts.MetaType = 'PCE';
+    MetaOpts.Input = myInput;                               % probability distribution for the uncertain variables
+    MetaOpts.FullModel = myModel_evals_windoff;    % the physical model as a UQLab object
+    MetaOpts.ExpDesign.NSamples = N_train;                  % 'experimental design' (ExpDesign): another word for 'training set'
+    if strcmp(MetaOpts.MetaType, 'Kriging')
+        MetaOpts.ExpDesign.Sampling = 'User';
+    end
+
+    flag_parfor = true;             % can we run the physical model in parallel to build the training set? (True/False)
+    seed = 100;                     % seed for reproducibility due to randomness in sampling the training set
+    N_train_increment = 8;          % we will increment the training set size until we reach convergence
+    N_train_max = 100;              % training budget (i.e., maximum number of training points allowed)
+    % run a test to check if surrogates are actually faster than classical MC for mean and sigma estimation  
+    % recommended only for cheap models (to find the true mean and sigma, we need a large MC with the physical model) 
+    flag_test_for_mean_and_sigma = false;
+    flag_test_set = false;          % will a test set be generated for further surrogate validation?
+
+    % Plots generator for parameter sweeps for the uncertain variables
+    inputs_name = ["Pitch angle (rad)"];     % list of the names of the uncertain variables
+    outputs_name = ["Mode 1 (Hz)", "Mode 2 (Hz)", "Mode 3 (Hz)", "Mode 4 (Hz)"];      % list of the names of the QIs
+    N_outputs = length(outputs_name);           % number of quantities of interest (QIs)
+    descriptive_title_for_plots = sprintf('%s surrogate (Young modulus E (Pa):%.2e)', MetaOpts.MetaType, alp(ii));
+    N_eval = 100;                                                        % number of discretisation points for each uncertain variable (for plots)
+    plotsfolderName = 'modal_frequencies_uncertain_pitch_angle'; 
+    subfolder_plotsfolderName = sprintf('Young_modulus_case_%u', ii); % each 'case' refers to one fixed combination of design variables
+    fullPath = fullfile(plotsfolderName, subfolder_plotsfolderName);
+    mkdir(fullPath);
+    mkdir(fullPath, 'plots_uq');
+    try
+        surrogates_modal_frequencies{ii, 1} =  surrogates_uq(MetaOpts, N_outputs, N_train_increment, N_train_max, flag_parfor, seed, fullPath, flag_test_for_mean_and_sigma, flag_test_set); % Generates training points and builds the surrogates 
+        elementToSave = surrogates_modal_frequencies{ii, 1}; 
+        save(fullfile(fullPath, sprintf('surrogate_modal_frequencies_Young_modulus_case_%u.mat', ii)), 'elementToSave'); % save the surrogate
+        uncertain_variables_exploration(elementToSave, inputs_name, outputs_name, descriptive_title_for_plots, N_eval, seed, fullPath); % plots generator using the surrogates 
+        % add readme to explain each 'case'
+        fileID = fopen(fullfile(fullPath, 'readme.txt'), 'w');
+        fprintf(fileID, 'Surrogates for each quantity of interest (QI) as a function of the uncertain variables (the deterministic variables are fixed).\n\n');
+        fprintf(fileID, 'Legend:\n');
+        N_outputs = length(outputs_name);             % number of quantities of interest (QIs)
+        N_variables = length(inputs_name);            % number of uncertain variables
+        for ll = 1:N_outputs
+            fprintf(fileID, 'QI %d: %s\n', ll, outputs_name(ll));
+        end   
+        for ll = 1:N_variables
+            fprintf(fileID, 'Uncertain variable %d: %s\n', ll, inputs_name(ll));
+        end    
+        fprintf(fileID, 'Deterministic variable 1: Young modulus; Case: %d out of %d; Value: %.5f (Pa)\n', ii, length(alp), alp(ii));
+        fprintf(fileID, 'Methodology: %s\n\n', descriptive_title_for_plots);
+        fprintf(fileID, 'The trained surrogates and most of the design exploration figures are stored externally due to size limits.\n'); 
+        fclose(fileID);
+        disp('Readme file for the surrogates has been created successfully.');
+    catch ME
+        fprintf('Error in sample (%d): %s\n', ii, ME.message);
+    end
+end    
+set(0, 'DefaultFigureVisible', 'on');
---
+++ example1/modal_frequencies_uncertain_pitch_angle/Young_modulus_case_2/readme.txt
@@ -0,0 +1,12 @@
+Surrogates for each quantity of interest (QI) as a function of the uncertain variables (the deterministic variables are fixed).
+
+Legend:
+QI 1: Mode 1 (Hz)
+QI 2: Mode 2 (Hz)
+QI 3: Mode 3 (Hz)
+QI 4: Mode 4 (Hz)
+Uncertain variable 1: Pitch angle (rad)
+Deterministic variable 1: Young modulus; Case: 2 out of 3; Value: 70000000000.00000 (Pa)
+Methodology: PCE surrogate (Young modulus E (Pa):7.00e+10)
+
+The trained surrogates and most of the design exploration figures are stored externally due to size limits.
