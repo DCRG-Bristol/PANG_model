a3cefa11c2206de0ef4a3d6cb6799c4605d6ca44
Valentin Breaz <valentin.breaz@bristol.ac.uk>
2026-02-05 10:13:38 +0000

    surrogates for flutter speed at different aoa (uncertain E and G)

---
+++ SJD_wing/flutter_first_hopf/aoa_case_3/readme.txt
@@ -0,0 +1,10 @@
+Surrogates for each quantity of interest (QI) as a function of the uncertain variables (the deterministic variables are fixed).
+
+Legend:
+QI 1: Flutter speed (m/s)
+Uncertain variable 1: Young's modulus (Pa)
+Uncertain variable 2: Torsion modulus (Pa)
+Deterministic variable 1: angle of attack; Case: 3 out of 3; Value: 0.02094 (rad)
+Methodology: Kriging surrogate (angle of attack(rad):2.09e-02)
+
+The trained surrogates and most of the design exploration figures are stored externally due to size limits.
--- SJD_wing/main_uq.m
+++ SJD_wing/main_uq.m
@@ -123,3 +123,81 @@ mkdir(fullPath, 'plots_uq');
 save(fullfile(fullPath, 'surrogate_tip_deflection_first_hopf_aoa_case_1_pce.mat'), 'elementToSave'); % save the surrogate
 uncertain_variables_exploration(elementToSave, inputs_name, outputs_name, descriptive_title_for_plots, N_eval, seed, fullPath); % plots generator using the surrogates 
 set(0, 'DefaultFigureVisible', 'on');
+
+%% 4. Surrogates for flutter speed (first Hopf point) (uncertain variables: E, G)
+% Description of the uncertain variables for UQLab
+InputOpts.Marginals(1).Type = 'Uniform';
+InputOpts.Marginals(1).Parameters = [0.9, 1.1]; % (uncertain scaling factor Young's modulus E) lower and upper uncertainty bound
+InputOpts.Marginals(2).Type = 'Uniform';
+InputOpts.Marginals(2).Parameters = [0.9, 1.1]; % (uncertain scaling factor modulus G for torsion) lower and upper uncertainty bound
+myInput = uq_createInput(InputOpts); 
+alp = ([0.3, 1.0, 1.2])*pi/180; %angles of attack to run...
+
+set(0, 'DefaultFigureVisible', 'off');
+surrogates_flutter_hopf = cell(length(alp), 1);
+
+for ii = 3:length(alp)
+    % Description of the physical model for UQLab
+    ModelOpts.mFile = 'model_flutter_speed_first_hopf';
+    ModelOpts.isVectorized = false;
+    ModelOpts.Parameters = [alp(ii)];
+    myModel_flutter_hopf = uq_createModel(ModelOpts);
+
+    N_train = 5;                                            % initial training set size (the set will be updated until the surrogate validation error is low enough)
+    MetaOpts.Type = 'Metamodel';                            % 'metamodel': another word for 'surrogate'    
+    MetaOpts.MetaType = 'Kriging';
+    MetaOpts.Input = myInput;                               % probability distribution for the uncertain variables
+    MetaOpts.FullModel = myModel_flutter_hopf;       % the physical model as a UQLab object
+    MetaOpts.ExpDesign.NSamples = N_train;                  % 'experimental design' (ExpDesign): another word for 'training set'
+    if strcmp(MetaOpts.MetaType, 'Kriging')
+        MetaOpts.ExpDesign.Sampling = 'User';
+    end
+
+    flag_parfor = true;             % can we run the physical model in parallel to build the training set? (True/False)
+    seed = 100;                     % seed for reproducibility due to randomness in sampling the training set
+    N_train_increment = 8;          % we will increment the training set size until we reach convergence
+    N_train_max = 1000;             % training budget (i.e., maximum number of training points allowed)
+    % run a test to check if surrogates are actually faster than classical MC for mean and sigma estimation  
+    % recommended only for cheap models (to find the true mean and sigma, we need a large MC with the physical model) 
+    flag_test_for_mean_and_sigma = false;
+    flag_test_set = true;          % will a test set be generated for further surrogate validation?
+
+    % Plots generator for parameter sweeps for the uncertain variables
+    inputs_name = ["Young's modulus (Pa)", "Torsion modulus (Pa)"];     % list of the names of the uncertain variables
+    outputs_name = ["Flutter speed (m/s)"];      % list of the names of the QIs
+    N_outputs = length(outputs_name);           % number of quantities of interest (QIs)
+    descriptive_title_for_plots = sprintf('%s surrogate (angle of attack(rad):%.2e)', MetaOpts.MetaType, alp(ii));
+    N_eval = 100;                                                        % number of discretisation points for each uncertain variable (for plots)
+    plotsfolderName = 'flutter_first_hopf'; 
+    subfolder_plotsfolderName = sprintf('aoa_case_%u', ii); % each 'case' refers to one fixed combination of design variables
+    fullPath = fullfile(plotsfolderName, subfolder_plotsfolderName);
+    mkdir(fullPath);
+    mkdir(fullPath, 'plots_uq');
+    try
+        surrogates_flutter_hopf{ii, 1} =  surrogates_uq(MetaOpts, N_outputs, N_train_increment, N_train_max, flag_parfor, seed, fullPath, flag_test_for_mean_and_sigma, flag_test_set); % Generates training points and builds the surrogates 
+        elementToSave = surrogates_flutter_hopf{ii, 1}; 
+        save(fullfile(fullPath, sprintf('surrogate_flutter_first_hopf_aoa_case_%u.mat', ii)), 'elementToSave'); % save the surrogate
+        uncertain_variables_exploration(elementToSave, inputs_name, outputs_name, descriptive_title_for_plots, N_eval, seed, fullPath); % plots generator using the surrogates 
+        % add readme to explain each 'case'
+        fileID = fopen(fullfile(fullPath, 'readme.txt'), 'w');
+        fprintf(fileID, 'Surrogates for each quantity of interest (QI) as a function of the uncertain variables (the deterministic variables are fixed).\n\n');
+        fprintf(fileID, 'Legend:\n');
+        N_outputs = length(outputs_name);             % number of quantities of interest (QIs)
+        N_variables = length(inputs_name);            % number of uncertain variables
+        for ll = 1:N_outputs
+            fprintf(fileID, 'QI %d: %s\n', ll, outputs_name(ll));
+        end   
+        for ll = 1:N_variables
+            fprintf(fileID, 'Uncertain variable %d: %s\n', ll, inputs_name(ll));
+        end    
+        fprintf(fileID, 'Deterministic variable 1: angle of attack; Case: %d out of %d; Value: %.5f (rad)\n', ii, length(alp), alp(ii));
+        fprintf(fileID, 'Methodology: %s\n\n', descriptive_title_for_plots);
+        fprintf(fileID, 'The trained surrogates and most of the design exploration figures are stored externally due to size limits.\n'); 
+        fclose(fileID);
+        disp('Readme file for the surrogates has been created successfully.');
+    catch ME
+        fprintf('Error in sample (%d): %s\n', ii, ME.message);
+    end
+end   
+set(0, 'DefaultFigureVisible', 'on');
+
